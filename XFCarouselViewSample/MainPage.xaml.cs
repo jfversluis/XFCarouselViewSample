cef74d2e24f4fc013c7e632dc06214502209e257
Gerald Versluis <gerald@verslu.is>
2021-03-21 14:07:53 +0100

    Implement CarouselView

--- XFCarouselViewSample/MainPage.xaml.cs
+++ XFCarouselViewSample/MainPage.xaml.cs
@@ -1,18 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace XFCarouselViewSample
 {
     public partial class MainPage : ContentPage
     {
+        private const string monkeyUrl = "https://montemagno.com/monkeys.json";
+        private readonly HttpClient httpClient = new HttpClient();
+
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new ObservableCollection<Monkey>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var monkeyJson = await httpClient.GetStringAsync(monkeyUrl);
+            var monkeys = JsonConvert.DeserializeObject<Monkey[]>(monkeyJson);
+
+            Monkeys.Clear();
+
+            foreach(var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
         }
     }
 }
---
+++ XFCarouselViewSample/Monkey.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace XFCarouselViewSample
+{
+    public class Monkey
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Location")]
+        public string Location { get; set; }
+
+        [JsonProperty("Details")]
+        public string Details { get; set; }
+
+        [JsonProperty("Image")]
+        public Uri Image { get; set; }
+
+        [JsonProperty("Population")]
+        public long Population { get; set; }
+
+        [JsonProperty("Latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("Longitude")]
+        public double Longitude { get; set; }
+    }
+}
